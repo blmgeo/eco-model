5c591219bae00acc3fd1a176ce2a3f1c120b5ab1
Brandon Mailhiot <brandon.mailhiot@gmail.com>
2016-12-06 18:29:42 -0500

    separated media queries from main css

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const container = document.querySelector('.container')
 title = document.querySelector('.title'),
-description = document.querySelector('.description'),
+quote = document.querySelector('.quote'),
 citation = document.querySelector('.citation'),
+explanation = document.querySelector('.explanation'),
 iframe = document.querySelector('iframe'),
 links = document.querySelectorAll('.nav-link'),
 list = document.querySelector('.nav-list'),
@@ -18,13 +19,13 @@ let updateNav = active => {
 
 let loadPen = () => {
   let slug = ''
-  const current = window.location.href.split('#')[1] || 'SteadyState'
+  const location = window.location.href.split('#')[1] || 'SteadyState'
 
-  switch (current) {
+  switch (location) {
     case 'SteadyState':
       updateNav(links[0])
       title.innerHTML = 'Steady State'
-      description.innerHTML = `
+      quote.innerHTML = `
       When the flow of a substance into a lake, the atmosphere, an animal, or any other "box" is equal
       to the outflow of that substance, then the amount, or "stock", of that substance in the box will
       be constant. This is called a "steady state" or "equilibrium". The ratio of the <em>stock</em> in the box
@@ -35,12 +36,15 @@ let loadPen = () => {
         - Consider a Spherical Cow
       </a> (Page 23)
       `
+      explanation.innerHTML = `
+
+      `
       slug = 'PbZZqq'
       break
     case 'NonSteadyState':
       updateNav(links[1])
       title.innerHTML = 'Non-Steady State'
-      description.innerHTML = `
+      quote.innerHTML = `
       [Steady state] problems &#8230; [are] solved by equating compartment inflows to compartment outflows &#8230; A more
       difficult class of problems involves stocks that change over time, a situation arising when inflows are not in
       balance with outflows. When the inflow, F<sub>in</sub>, is not equal to the outflow F<sub>out</sub>, then the rate
@@ -52,12 +56,15 @@ let loadPen = () => {
         - Consider a Spherical Cow
       </a> (Page 111)
       `
+      explanation.innerHTML = `
+
+      `
       slug = 'oYbbXm'
       break
     case 'Diversity':
       updateNav(links[2])
       title.innerHTML = 'Diversity'
-      description.innerHTML = `
+      quote.innerHTML = `
       A diversity index is a quantitative measure that reflects how many different types (such as species)
       there are in a dataset, and simultaneously takes into account how evenly the basic entities (such as individuals)
       are distributed among those types. The value of a diversity index increases both when the number of types increases
@@ -69,12 +76,15 @@ let loadPen = () => {
         - Wikipedia
       </a> (Diversity index)
       `
+      explanation.innerHTML = `
+
+      `
       slug = 'pNggjw'
       break
     case 'Gompertz':
       updateNav(links[3])
       title.innerHTML = 'Gompertz'
-      description.innerHTML = `
+      quote.innerHTML = `
       A Gompertz curve or Gompertz function, named after Benjamin Gompertz, is a sigmoid function. It is a type of mathematical model
       for a time series, where growth is slowest at the start and end of a time period. The right-hand or future value asymptote
       of the function is approached much more gradually by the curve than the left-hand or lower valued asymptote, in contrast to the simple
@@ -85,12 +95,15 @@ let loadPen = () => {
         - Wikipedia
       </a> (Gompertz function)
       `
+      explanation.innerHTML = `
+
+      `
       slug = 'KNVVVm'
       break
     case 'Lotka-Volterra':
       updateNav(links[4])
       title.innerHTML = 'Lotka-Volterra'
-      description.innerHTML = `
+      quote.innerHTML = `
       The Lotka-Volterra equations describe an ecological predator-prey (or parasite-host) model which assumes that, for a set of fixed positive
       constants A (the growth rate of prey), B (the rate at which predators destroy prey), C (the death rate of predators), and D
       (the rate at which predators increase by consuming prey), &#8230; a prey population <em>x</em> increases at a rate <em>dx=Axdt</em>
@@ -102,6 +115,9 @@ let loadPen = () => {
       <a target='_blank' class='ref' href='http://mathworld.wolfram.com/Lotka-VolterraEquations.html'>
         - Wolfram Mathworld
       </a> (Lotka-Volterra Equations)
+      `
+      explanation.innerHTML = `
+
       `
       slug = 'Rorrab'
       break
