b6086c52ad203cb91f4c2806fc03e6b60bd87eed
Brandon Mailhiot <brandon.mailhiot@gmail.com>
2016-12-12 16:12:30 -0500

    bundled modules

---
+++ src/lib/EcoModel.js
@@ -0,0 +1,13 @@
+var Diversity = require('../Diversity'),
+Gompertz = require('../Gompertz'),
+Lotka_Volterra = require('../Lotka-Volterra'),
+NonSteadyState = require('../NonSteadyState'),
+SteadyState = require('../SteadyState')
+
+module.exports = {
+  Diversity,
+  Gompertz,
+  Lotka_Volterra,
+  NonSteadyState,
+  SteadyState
+}
